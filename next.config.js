680dcede258a55232c4db9719eda7880948b433c
Manrich121 <manrich121@gmail.com>
2025-10-10 14:49:14 +0200

    chore: bump nextjs 16 beta and next-auth 5 beta

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true
+  }
+};
 
 module.exports = nextConfig;
