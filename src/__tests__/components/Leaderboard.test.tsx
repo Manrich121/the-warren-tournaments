e51fcfe42e89de6eba831257c3143d2efde124fa
Manrich121 <manrich121@gmail.com>
2025-11-08 13:45:01 +0200

    move tests

--- src/__tests__/components/Leaderboard.test.tsx
+++ src/__tests__/components/Leaderboard.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import Leaderboard from '@/components/Leaderboard';
 import { LeaderboardEntry } from '@/types/leaderboard';
 
